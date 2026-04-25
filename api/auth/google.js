ff0a248754ec0700a367491910f121cfbebdc643
vexolirity <yusufhisyamaa@gmail.com>
2026-04-25 11:46:11 +0700

    Update google.js

--- api/auth/google.js
+++ api/auth/google.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
     const CLIENT_ID = '75728450214-1q4ravi8pjveco1ruiao3k6qrnvdlfk1.apps.googleusercontent.com';
     const REDIRECT_URI = `https://${req.headers.host}/api/auth/callback`;
     
-    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=email%20profile&access_type=online`;
+    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=email%20profile`;
     
     res.redirect(url);
 }
