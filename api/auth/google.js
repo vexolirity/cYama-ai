1d0cf9df0fd0021990b8b297745a96e656352087
vexolirity <yusufhisyamaa@gmail.com>
2026-04-25 13:32:25 +0700

    Update google.js

--- api/auth/google.js
+++ api/auth/google.js
@@ -1,6 +1,11 @@
 export default function handler(req, res) {
-    const CLIENT_ID = '75728450214-1q4ravi8pjveco1ruiao3k6qrnvdlfk1.apps.googleusercontent.com';
+    const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
     const REDIRECT_URI = `https://${req.headers.host}/api/auth/callback`;
+    
+    if (!CLIENT_ID) {
+        console.error('❌ GOOGLE_CLIENT_ID tidak ditemukan!');
+        return res.status(500).send('Konfigurasi Google Login error');
+    }
     
     const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=email%20profile`;
     
